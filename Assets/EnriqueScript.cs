e7f3335ec444f1f8ff182036f8dda1ea04bbe66b
fernandopaniagua <fernando.paniagua@gmail.com>
2018-12-14 11:21:06 +0100

    Initial commit

---
+++ Assets/EnriqueScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class EnriqueScript : MonoBehaviour
+{
+    DatabaseReference reference;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://demofirebasegetafe.firebaseio.com/");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        FirebaseDatabase.DefaultInstance.GetReference("enrique").ValueChanged += Mover;
+    }
+
+
+
+    void Mover(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+        IEnumerable<DataSnapshot> posiciones = args.Snapshot.Children;
+        foreach (var coordenada in posiciones)
+        {
+            if (coordenada.Key.ToString() == "x")
+            {
+                transform.position = new Vector3(int.Parse(coordenada.Value.ToString()), transform.position.y);
+            }
+            else if (coordenada.Key.ToString() == "y")
+            {
+                transform.position = new Vector3(transform.position.x, int.Parse(coordenada.Value.ToString()));
+            }
+        }
+
+    }
+
+}
---
+++ Assets/FernandoScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class FernandoScript : MonoBehaviour {
+    DatabaseReference reference;
+
+   
+	// Use this for initialization
+	void Start () {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://demofirebasegetafe.firebaseio.com/");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 1, 0);
+            reference.Child("fernando/y").SetValueAsync(transform.position.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -1, 0);
+            reference.Child("fernando/y").SetValueAsync(transform.position.y);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-1, 0, 0);
+            reference.Child("fernando/x").SetValueAsync(transform.position.x);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(1, 0, 0);
+            reference.Child("fernando/x").SetValueAsync(transform.position.x);
+
+        }
+    }
+
+}
